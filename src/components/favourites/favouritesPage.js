7705d06c05f87c7c9ad7fdbd1b78dff91e28f92f
Hammad Mukhtar <hammadmukhtar1122@gmail.com>
2023-07-29 20:50:58 +0500

    ..

--- src/components/favourites/favouritesPage.js
+++ src/components/favourites/favouritesPage.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
 const FavouritesPage = () => {
-  const favourites = useSelector((state) => state.favourites) || [];
+  const favorites = useSelector((state) => state.favorite.value);
 
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -3,20 +3,14 @@ import "./index.css";
 import App from "./App";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import store from "./components/favourites/store";
-// import { Provider } from "react-redux";
-// import { PersistGate } from "redux-persist/integration/react";
-// import { persistStore } from "redux-persist";
-
-// let persistor = persistStore(store);
+import { store } from "./components/favourites/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-    {/* <PersistGate loading={null} persistor={persistor}> */}
-    <App />
-    {/* </PersistGate> */}
-    {/* </Provider> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
